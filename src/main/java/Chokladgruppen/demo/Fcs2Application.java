f4d32c3b6e76c33e7808cbbd6ce47343282c245b
Marcstro <marcus.strom.90@gmail.com>
2019-05-20 10:58:24 +0200

    StartatProjektet  //marcus

---
+++ src/main/java/Chokladgruppen/demo/Fcs2Application.java
@@ -0,0 +1,15 @@
+package Chokladgruppen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fcs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fcs2Application.class, args);
+                
+                System.out.println("MARCUS HAS MADE A PROJECT!");
+	}
+
+}
