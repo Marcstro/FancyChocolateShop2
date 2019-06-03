68e1a6a4fa52831f9631f36ad2be6c2fee849bdc
Jenny Östman <jenny.ostman@yh.nackademin.se>
2019-06-03 10:53:47 +0200

    popups fungerar!

--- src/main/resources/static/js/productsScript.js
+++ src/main/resources/static/js/productsScript.js
@@ -72,63 +72,61 @@ const chocolateCardsDiv = document.querySelector("#divForChocolateCards");
 // Element att lägga popUpContent i
 const popUpContentDiv = document.querySelector("#popUpContent");
 
+
 // Skapar html för pop up
-// Behöver nog ha med sig ett id här. Använder ett hårdkodat nu
-var id = 1;
-var index = -1;
 function addPopUpContent(id) {
-//    console.log("Inne i metoden!");
-
     listOfProducts.map((x, i) => {
-//        console.log("Inne i map!");
-
-        if (x.id === 1) {
-//            console.log("Sätter index!");
-            index = 1;
+        if (x.id === id) {
+            console.log("inne i addPopUpContent - id: " + id)
             popUpContentDiv.innerHTML += `<div class="row">`
-                + `<div class="col s6 m2 l2 xl2">`
-                + `<img src="images/${x.smallPictureName}">`
-                + `</div>`
-                + `</div>`
-                + `<div class="row">`
-                + `<div class="col s12 m10 l10 xl10 ">`
-                + `<h4 class="cardTitleFont">${x.name}</h4>`
-                + `<h6 class=dreamyFont3>${x.brand}</h6>`
-                + `<p>${x.description}</p>`
-                + `</div>`
-                + `</div>`;
-            console.log("x: " + JSON.stringify(x));
+                    + `<div class="col s6 m2 l2 xl2">`
+                    + `<img src="images/${x.smallPictureName}">`
+                    + `</div>`
+                    + `</div>`
+                    + `<div class="row">`
+                    + `<div class="col s12 m10 l10 xl10 ">`
+                    + `<h4 class="cardTitleFont">${x.name}</h4>`
+                    + `<h6 class=dreamyFont3>${x.brand}</h6>`
+                    + `<p>${x.description}</p>`
+                    + `</div>`
+                    + `</div>`;
         }
     });
-
-
 }
+
 
 // Skapar html för chocolateCards
 function addChocolateCardElements() {
-    VariableNamesArray = [];
-
     listOfProducts.map((x, i) => {
         chocolateCardsDiv.innerHTML += `<div class="col s4 m4">`
-            + `<div class="card">`
-            + `<div class="card-image">`
-            + `<img src="images/${x.smallPictureName}">`
-            + `<span class="card-title cardTitleFont">${x.name}</span>`
-            + `<a id="modalLink${i}" class="btn-floating halfway-fab waves-effect waves-light pink lighten-4 modal-trigger" href="#modal1">`
-            + `<i class="material-icons">more_horiz</i></a>`
-            + `</div>`
-            + `<div class="card-content">`
-            + `<p>${x.description}</p>`
-            + `</div>`
-            + `</div>`
-            + `</div>`;
-
-        // Lägger till actionListener till länk som öppnar popUp
-        // Detta ger sista kortet info på popupen, pga listenerElement sparas över
-        var listenerElement = document.getElementById(`modalLink${i}`);
-        listenerElement.addEventListener("click", addPopUpContent);
-
+                + `<div class="card">`
+                + `<div class="card-image">`
+                + `<img src="images/${x.smallPictureName}">`
+                + `<span class="card-title cardTitleFont">${x.name}</span>`
+                + `<a id="modalLink${i}" class="btn-floating halfway-fab waves-effect waves-light pink lighten-4 modal-trigger" href="#modal1">`
+                + `<i class="material-icons">more_horiz</i></a>`
+                + `</div>`
+                + `<div class="card-content">`
+                + `<p>${x.description}</p>`
+                + `</div>`
+                + `</div>`
+                + `</div>`;
     });
+    addEventListenersToChocolateCards();
+}
 
+
+//  Array för att skapa dynamiska eventListener-variabler
+var listenerElement = [];
+// Lägger till actionListerners efter att html-koden för alla kort genererats
+function addEventListenersToChocolateCards() {
+    listOfProducts.map((x, i) => {
+        listenerElement.push("listenerElement" + i);
+        listenerElement[i] = document.getElementById(`modalLink${i}`);
+        console.log("Id1: " + listOfProducts[i].id);
+        listenerElement[i].addEventListener("click", function () {
+            addPopUpContent(x.id);
+        });
+    });
 
 }
